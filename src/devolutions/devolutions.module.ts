63710adde7de41eff2a550774c94f28295258d8d
Javier Laris <flopezlaris@gmail.com>
2025-11-10 19:56:25 -0700

    fix send emails

--- src/devolutions/devolutions.module.ts
+++ src/devolutions/devolutions.module.ts
@@ -3,11 +3,11 @@ import { DevolutionsService } from './devolutions.service';
 import { DevolutionsController } from './devolutions.controller';
 import { Devolution } from 'src/entities/devolution.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ChargeShipment, Income, Shipment } from 'src/entities';
+import { ChargeShipment, Income, Shipment, Subsidiary } from 'src/entities';
 import { MailService } from 'src/mail/mail.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Devolution, Shipment, Income, ChargeShipment])],
+  imports: [TypeOrmModule.forFeature([Devolution, Shipment, Income, ChargeShipment, Subsidiary])],
   controllers: [DevolutionsController],
   providers: [DevolutionsService, MailService],
 })
