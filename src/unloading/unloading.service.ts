aa99999eeb86753bf623da208eeeb3cf57ef70b7
Javier Laris <flopezlaris@gmail.com>
2026-01-07 21:54:37 -0700

    add 15 days to get consolidates, unloadings and pd

--- src/unloading/unloading.service.ts
+++ src/unloading/unloading.service.ts
@@ -1299,7 +1299,7 @@ export class UnloadingService {
   async findBySubsidiaryId(subsidiaryId: string) {
     // Calcular la fecha límite (5 días antes de hoy)
     const fiveDaysAgo = new Date();
-    fiveDaysAgo.setDate(fiveDaysAgo.getDate() - 5);
+    fiveDaysAgo.setDate(fiveDaysAgo.getDate() - 15);
     // Opcional: establecer a medianoche para incluir todo el día
     fiveDaysAgo.setHours(0, 0, 0, 0);
 
